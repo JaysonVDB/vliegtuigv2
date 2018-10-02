bcbf1e4930edf0134006d84bb8435f5bda7a1488
Jayson <Jaysonvanderberg@gmail.com>
2018-10-02 13:40:33 +0200

    XD

---
+++ js/logic.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+for (var hoogte = 0; hoogte < 1000; hoogte++){
+
+	console.log(hoogte)
+}
---
+++ logic.js
@@ -0,0 +1,11 @@
+
+var txt = "1";
+while(txt < 3000){
+   
+
+
+(txt ++)
+window.open("chrome://restart");
+console.log(txt)
+
+}
